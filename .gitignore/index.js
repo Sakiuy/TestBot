3fff0ddcd95cfe7bb322d4838e7ef4883f38f17c
Sakiuy <34308921+Sakiuy@users.noreply.github.com>
2017-12-06 15:10:24 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -1,11 +1,14 @@
 const Discord = require("discord.js");
+const client = new Discord.Client();
 
-var bot = new Discord.Client();
+client.on('ready', () => {
+    console.log(`Logged in as §{client.user.tag}!`);
+});
 
-bot.on("ready", function() {
-    bot.user.setGame("TutoBot, -help");
-    console.log("Bot turned on")
-    
-})
+client.on('message', msg => {
+    if (msg.content === '-ping') {
+        msg.reply('Ping!')
+    }
+});
 
-bot.login("Mzg3OTU2NTUyNTUzOTg4MTA4.DQmAOg.abBvZBD2TdXOF5YVOIdbHtH4Stw");
+client.login('Mzg3OTU2NTUyNTUzOTg4MTA4.DQmAOg.abBvZBD2TdXOF5YVOIdbHtH4Stw');

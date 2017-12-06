af80937297a0ea2a5e19acf7475ae19e622c84a9
Sakiuy <34308921+Sakiuy@users.noreply.github.com>
2017-12-06 16:33:09 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -5,9 +5,16 @@ client.on('ready', () => {
     console.log(`Logged in as §{client.user.tag}!`);
 });
 
-client.on('message', msg => {
-    if (msg.content === '-ping') {
-        msg.reply('Ping!')
+client.on('message', message => {
+    if (message.equals === '-ping') {
+        message.channel.send('Pong!')
+    }
+
+    if (message.content === '-help') {
+        message.channel.send("►ZENOXIA◄")
+        message.channel.send("-help : Afficher l'aide")
+        message.channel.send("-ping : Verifier la vitesse du bot")
+        
     }
 });
 
